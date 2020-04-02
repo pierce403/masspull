5c58a22e90290fbaa199a67600586df757bf8031
Dean Pierce <pierce403@gmail.com>
2020-04-01 20:54:14 -0700

    add the data link

--- run-server.sh
+++ run-server.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-
 export LC_ALL="C.UTF-8"
 export LANG="C.UTF-8"
 export FLASK_APP=./masspull_server.py
 
+if [ ! -d venv ]
+then
+    echo "[+] Creating new python3 virtualenv named venv"
+    virtualenv -p /usr/bin/python3 venv
+fi
+
 source venv/bin/activate
+pip3 install flask
+
 while [ 1 == 1 ]
 do
   echo `date` >> start.log
-  flask run --with-threads # --host=0.0.0.0
+  flask run --with-threads --host=0.0.0.0
   sleep 5
 done
 
